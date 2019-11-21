82ed2ca015ffc8fcdad3f96c0067a8f31632deb8
Emanuel Estrada Larios <emamex98@users.noreply.github.com>
2019-11-21 10:37:28 -0600

    Add files

---
+++ Session3/google-kb.go
@@ -0,0 +1,47 @@
+package main 
+
+import (
+	"io/ioutil" 
+	"log" 
+	"net/http" 
+	"encoding/json"
+)
+
+type result struct {
+	Name string
+	Description string
+}
+
+type itemListElement struct {
+	ResultScore float32
+	Result result
+}
+
+type gkb struct {
+	ItemListElement []itemListElement
+}
+
+func main() { 
+
+	resp, err := http.Get("https://kgsearch.googleapis.com/v1/entities:search?query=tecnologico+de+monterrey&key=[ApiKey]&indent=True") 
+
+	if err != nil { 
+		log.Fatalln(err) 
+	} 
+
+	defer resp.Body.Close() 
+
+	body, err := ioutil.ReadAll(resp.Body) 
+	if err != nil { 
+		log.Fatalln(err) 
+	}
+
+	var bodyJsonString string = string(body)
+	var resultStruct gkb
+
+	json.Unmarshal([]byte(bodyJsonString), &resultStruct)
+
+	log.Println("Google Knowledge Graph knows that", resultStruct.ItemListElement[0].Result.Name, "is a", resultStruct.ItemListElement[0].Result.Description, "with an accuracy score of", resultStruct.ItemListElement[0].ResultScore)
+
+
+}
